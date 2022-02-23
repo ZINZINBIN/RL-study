a429b805d61d43e584689b8e54fc19739eb43ba4
zinzinbin <asdwlstn@naver.com>
2022-02-23 09:15:13 +0000

    add test file

---
+++ DQN-Atari/bot2random.py
@@ -0,0 +1,38 @@
+import gym
+import random
+from pyvirtualdisplay import Display
+
+GAME_LIST = [
+    'SpaceInvaders-v0',
+    'Breakout-v0'
+]
+
+def main():
+    display = Display(visible=False, size = (400,300))
+    display.start()
+
+    env = gym.make(GAME_LIST[0]).unwrapped
+    env.reset()
+
+    n_iter = 0
+    max_iters = 10000
+
+    episode_reward = 0
+
+    while True:
+        n_iter +=1
+        action = env.action_space.sample()
+        _, reward, done, _ = env.step(action)
+        episode_reward += reward
+
+        if done:
+            print('Reward: %s' % episode_reward)
+            break
+            
+        if n_iter >= max_iters:
+            break
+
+    env.close()
+    
+if __name__ == '__main__':
+    main()
