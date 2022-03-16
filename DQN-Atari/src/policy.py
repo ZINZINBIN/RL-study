6bde285cd8079f6d5a77304ff409697726bee313
zinzinbin <asdwlstn@naver.com>
2022-03-16 05:54:49 +0000

    add A3C algorithm

--- DQN-Atari/src/policy.py
+++ DQN-Atari/src/policy.py
@@ -6,3 +6,103 @@
 - PPO
 - TPGO
 '''
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.multiprocessing as mp
+import numpy as np
+from torch.distributions import Categorical
+from pytorch_model_summary import summary
+
+class ActorCritic(nn.Module):
+    def __init__(self, h, w, n_actions, hidden_dims = 128):
+        super(ActorCritic, self).__init__()
+
+        h1 = self._conv2d_size_out(h, kernel_size = 8, stride = 4)
+        w1 = self._conv2d_size_out(w, kernel_size = 8, stride = 4)
+
+        h2 = self._conv2d_size_out(h1, kernel_size = 4, stride = 2)
+        w2 = self._conv2d_size_out(w1, kernel_size = 4, stride = 2)
+
+        h3 = self._conv2d_size_out(h2, kernel_size = 3, stride = 1)
+        w3 = self._conv2d_size_out(w2, kernel_size = 3, stride = 1)
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size = 8, stride = 4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.n1 = nn.LayerNorm([32,h1,w1])
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 4, stride = 2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.n2 = nn.LayerNorm([64, h2, w2])
+        self.conv3 = nn.Conv2d(64,64,kernel_size = 3, stride = 1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.n3 = nn.LayerNorm([64, h3, w3])
+
+        convw = self._conv2d_size_out(self._conv2d_size_out(self._conv2d_size_out(w, kernel_size = 8, stride = 4), 4, 2), 3, 1)
+        convh = self._conv2d_size_out(self._conv2d_size_out(self._conv2d_size_out(h, kernel_size = 8, stride = 4), 4, 2), 3, 1)
+        linear_input_dim = h3 * w3 * 64
+
+        self.hidden_dims = hidden_dims
+        self.n_actions = n_actions
+
+        # policy distirbution : pi(a|s)
+        self.actor = nn.Sequential(
+            nn.Linear(linear_input_dim, hidden_dims),
+            nn.ReLU(),
+            nn.Linear(hidden_dims, n_actions)
+        )
+        # value : V(s)
+        self.critic = nn.Sequential(
+            nn.Linear(linear_input_dim, hidden_dims),
+            nn.ReLU(),
+            nn.Linear(hidden_dims, 1)
+        )
+
+        # init encoder
+        self.init_weights()
+        
+    def _conv2d_size_out(self, size, kernel_size = 5, stride = 2):
+        outputs = (size - (kernel_size - 1) - 1) // stride + 1
+        return outputs
+
+    def forward(self, x):
+        # Encoding and Decoding
+        # x = nn.functional.relu(self.bn1(self.conv1(x)))
+        # x = nn.functional.relu(self.bn2(self.conv2(x)))
+        # x = nn.functional.relu(self.bn3(self.conv3(x)))
+
+        x = nn.functional.relu(self.n1(self.conv1(x)))
+        x = nn.functional.relu(self.n2(self.conv2(x)))
+        x = nn.functional.relu(self.n3(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+
+        # Value approximation
+        value = self.critic(x)
+
+        # Policy Distribution
+        policy = nn.functional.softmax(self.actor(x))
+        dist = Categorical(policy)
+
+        return value, dist
+
+    def summary(self, sample_inputs):
+        print(summary(self, sample_inputs, max_depth = None, show_parent_layers=True, show_input = True))
+
+    def init_weights(self):
+        # xavier init
+        torch.nn.init.xavier_uniform_(self.conv1.weight)
+        torch.nn.init.xavier_uniform_(self.conv2.weight)
+        torch.nn.init.xavier_uniform_(self.conv3.weight)
+
+        nn.init.constant_(self.conv1.bias, 0.0)
+        nn.init.constant_(self.conv2.bias, 0.0)
+        nn.init.constant_(self.conv3.bias, 0.0)
+
+        # normalalization
+        # nn.init.normal_(self.conv1.weight, mean = 0, std = 0.1)
+        # nn.init.normal_(self.conv2.weight, mean = 0, std = 0.1)
+        # nn.init.normal_(self.conv3.weight, mean = 0, std = 0.1)
+
+# DPG algorithm
+
+# DDPG algorithm
---
+++ DQN-Atari/train_a3c.py
@@ -0,0 +1,241 @@
+import gym
+import random
+import math
+import wandb
+import gc
+from torch import long
+import torch
+import matplotlib.pyplot as plt
+import argparse
+from itertools import count
+from src.utility import *
+from src.model import *
+from src.buffer import PrioritzedReplayMemory, ReplayMemory, Transition
+from src.multiprocessing_env import *
+from src.policy import *
+from pyvirtualdisplay import Display
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description="training atari with A3C method")
+parser.add_argument("--hidden_dims", type = int, default = 128)
+parser.add_argument("--lr", type = float, default = 2e-5)
+parser.add_argument("--weight_decay", type = float, default = 0.95)
+parser.add_argument("--max_grad_norm", type = float, default = 1.0)
+parser.add_argument("--gamma", type = float, default = 0.95)
+parser.add_argument("--n_steps", type = int, default = 12)
+parser.add_argument("--gpu_num", type = int, default = 0)
+parser.add_argument("--max_frame", type = int, default = 10000)
+parser.add_argument("--wandb_save_name", type = str, default = "A3C-exp001")
+parser.add_argument("--value_loss_coeff", type = float, default = 1.0)
+parser.add_argument("--policy_loss_coeff", type = float, default = 0.5)
+parser.add_argument("--entropy_loss_coeff", type = float, default = 0.01)
+
+
+args = vars(parser.parse_args())
+
+hidden_dims = args["hidden_dims"]
+lr = args["lr"]
+weight_decay = args["weight_decay"]
+n_steps = args["n_steps"]
+max_frame = args['max_frame']
+gamma = args['gamma']
+value_loss_coeff = args['value_loss_coeff']
+policy_loss_coeff = args['policy_loss_coeff']
+entropy_loss_coeff = args['entropy_loss_coeff']
+max_grad_norm = args['max_grad_norm']
+
+display = Display(visible=False, size = (400,300))
+display.start()
+
+env = gym.make('Breakout-v0').unwrapped
+env.reset()
+
+n_actions = env.action_space.n
+
+# cuda check
+if torch.cuda.is_available():
+    print("cuda available : ", torch.cuda.is_available())
+    print("cuda device count : ", torch.cuda.device_count())
+    device = "cuda:" + str(args["gpu_num"])
+else:
+    device = "cpu" 
+
+# initialize screen
+init_screen = get_screen(env)
+_,_,screen_height, screen_width = init_screen.shape
+
+# Network Loaded
+a3c = ActorCritic(screen_height, screen_width, n_actions, hidden_dims)
+a3c.to(device)
+
+# model summary
+sample_inputs = torch.zeros_like(init_screen, device=device)
+a3c.summary(sample_inputs)
+
+# Opimizer
+optimizer = torch.optim.RMSprop(a3c.parameters(), lr = lr, weight_decay=weight_decay)
+
+# compute returns
+def compute_returns(next_value, rewards, masks, gamma = 0.99):
+    R = next_value
+    returns = []
+
+    for step in reversed(range(len(rewards))):
+        R = rewards[step] + gamma * R * masks[step]
+        returns.insert(0, R) # 0번째 자리에 R 값을 집어넣는다(삽입)
+
+    return returns
+
+# test environment
+def test_env():
+    env.reset()
+    state = get_screen(env)
+    done = False
+    total_reward = 0
+    a3c.eval()
+
+    while not done:
+        with torch.no_grad():
+            state = state.to(device)
+            value, dist = a3c(state)
+            action = dist.sample()
+            _, reward, done, _ = env.step(action.item())
+            total_reward += reward
+            next_state = get_screen(env)
+            state = next_state
+
+    return total_reward
+
+train_losses = []
+test_rewards = []
+test_frames = []
+
+if __name__ == "__main__":
+
+    # # wandb initialized
+    # wandb.init(project="DQN-Atari", entity="zinzinbin")
+
+    # # wandb experiment name edit
+    # wandb.run.name = args["wandb_save_name"]
+
+    # # save run setting
+    # wandb.run.save()
+
+    # wandb setting
+    # wandb.config = {
+    #     "learning_rate": lr,
+    #     "episode": num_episode,
+    #     "target_update":target_update,
+    #     "hidden_dims":hidden_dims,
+    #     "n_steps":n_steps,
+    #     "momentum":momentum,
+    #     "optimizer":"RMSprops"
+    # }
+
+    # training process for each episode
+    for frame_idx in tqdm(range(max_frame)):
+        env.reset()
+        state = get_screen(env)
+
+        log_probs = []
+        rewards = []
+        masks = []
+        values = []
+        losses = []
+        entropy = 0
+
+        # multi-steps  A3C algorithm : to avoid strong correlation between samples
+        for n_step in range(n_steps):
+            state = state.to(device)
+            value, dist = a3c(state)
+            action = dist.sample()
+            _, reward, done, _ = env.step(action.item())
+
+            reward = torch.tensor([reward], device = device)
+            
+            if done:
+                break
+            else:
+                next_state = get_screen(env)
+
+            state = next_state
+
+            log_prob = dist.log_prob(action)
+            entropy += dist.entropy().mean()
+
+            log_probs.append(log_prob)
+            values.append(value)
+            rewards.append(reward.unsqueeze(1))
+            masks.append(torch.tensor([1-int(done)], device = device).unsqueeze(1))
+
+        # test plot
+        if frame_idx % 100 == 0:
+            test_reward = test_env()
+            print("frame_idx : {}, test_reward : {:.3f}".format(frame_idx, test_reward))
+            test_rewards.append(test_reward)
+            test_frames.append(frame_idx)
+
+        # next_state에 대한 예측값 
+        log_probs = torch.cat(log_probs).to(device)
+        next_state = next_state.to(device)
+        next_value, next_dist = a3c(next_state)
+        next_value = next_value.item()
+    
+        returns = compute_returns(next_value, rewards, masks, gamma)
+
+        returns = torch.cat(returns).detach()
+        values = torch.cat(values)
+
+        advantage = returns - values
+        policy_loss = -(log_probs * advantage.detach()).mean()
+        value_loss = advantage.pow(2).mean()
+
+        loss = policy_loss_coeff * policy_loss + value_loss_coeff * value_loss - entropy_loss_coeff * entropy
+        optimizer.zero_grad()
+        
+        # gradient clipping 
+        torch.nn.utils.clip_grad_norm_(a3c.parameters(), max_grad_norm)
+
+        loss.backward()
+        optimizer.step()
+
+        train_losses.append(loss.item())
+        
+        # wandb.log({
+        #     "episode_duration":len(episode_durations),
+        #     "mean_loss":mean_loss, 
+        #     "sum_loss":sum_loss,
+        #     "mean_reward":mean_reward,
+        #     "sum_reward":sum_reward
+        # })
+
+        # memory cache delete
+        gc.collect()
+
+        # torch cache delete
+        torch.cuda.empty_cache()
+
+    print("training policy network and target network done....!")
+
+    env.close()
+
+    # plot the result
+    plt.subplot(1,2,1)
+    plt.plot(range(1, max_frame + 1), train_losses, 'r--', label = 'train loss')
+    plt.xlabel("Episode")
+    plt.ylabel("Loss")
+    plt.ylim([0, 1.0])
+    plt.legend()
+
+    plt.subplot(1,2,2)
+    plt.plot(test_frames, test_rewards, 'bo--', label = 'test reward')
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.legend()
+
+    plt.savefig("./results/A3C_loss_reward_curve.png")
+
+    weights_save_dir = "./weights/a3c_" + args['wandb_save_name'].split("-")[1] + ".pt"
+
+    # save model weights 
+    torch.save(a3c.state_dict(), weights_save_dir)
